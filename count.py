2a1e009fb0fe1f4c6069b8b41c6502fa592121be
nickname0q <129134147+nickname0q@users.noreply.github.com>
2023-05-20 07:49:36 +0300

    Update count.py
    rewritten on friendly-telegram

--- count.py
+++ count.py
@@ -1,55 +1,61 @@
-from telethon import events
-from datetime import datetime
-from telethon.tl.types import User, Chat, Channel
-from uniborg.util import admin_cmd
+import datetime
+from telethon import types
+from .. import loader, utils
 
+@loader.tds
+class CountDialogsMod(loader.Module):
+    """Counts the number of dialogs in your Telegram account."""
 
-@borg.on(admin_cmd(pattern="count"))
-async def count_dialogs(event):
-    if event.fwd_from:
-        return
+    strings = {"name": "CountDialogs"}
 
-    start_time = datetime.now()
+    def __init__(self):
+        self.client = None
 
-    num_users = 0
-    num_basic_groups = 0
-    num_super_groups = 0
-    num_channels = 0
-    num_bots = 0
-    num_blocked_chats = 0
+    async def client_ready(self, client, db):
+        self.client = client
 
-    await event.edit("Getting information...")
+    async def countcmd(self, message):
+        """Counts the number of dialogs in your Telegram account."""
+        start_time = datetime.datetime.now()
 
-    async for dialog in borg.iter_dialogs():
-        if isinstance(dialog.entity, User):
-            if dialog.entity.bot:
-                num_bots += 1
-            else:
-                num_users += 1
-        elif isinstance(dialog.entity, Channel):
-            if dialog.entity.broadcast:
-                num_channels += 1
-            else:
-                num_super_groups += 1
-        elif isinstance(dialog.entity, Chat):
-            num_basic_groups += 1
-            try:
-                await borg.get_permissions(dialog)
-            except:
-                num_blocked_chats += 1
-        else:
-            logger.info(dialog.stringify())
+        num_users = 0
+        num_basic_groups = 0
+        num_super_groups = 0
+        num_channels = 0
+        num_bots = 0
+        num_blocked_chats = 0
 
-    end_time = datetime.now()
-    duration_in_seconds = (end_time - start_time).seconds
+        await utils.answer(message, "Getting information...")
 
-    await event.edit(
-        f"My Telegram Account Infos:\n"
-        f"Users:\t{num_users}\n"
-        f"Basic Groups:\t{num_basic_groups}\n"
-        f"Super Groups:\t{num_super_groups}\n"
-        f"Channels:\t{num_channels}\n"
-        f"Bots:\t{num_bots}\n"
-        f"Blocked Chats:\t{num_blocked_chats}\n"
-        f"Obtained in {duration_in_seconds} seconds."
-    )
+        async for dialog in self.client.iter_dialogs():
+            if isinstance(dialog.entity, types.User):
+                if dialog.entity.bot:
+                    num_bots += 1
+                else:
+                    num_users += 1
+            elif isinstance(dialog.entity, types.Channel):
+                if dialog.entity.broadcast:
+                    num_channels += 1
+                else:
+                    num_super_groups += 1
+            elif isinstance(dialog.entity, types.Chat):
+                num_basic_groups += 1
+                try:
+                    await self.client.get_permissions(dialog)
+                except:
+                    num_blocked_chats += 1
+
+        end_time = datetime.datetime.now()
+        duration_in_seconds = (end_time - start_time).seconds
+
+        await utils.answer(
+            message,
+            f"My Telegram Account Infos:\n"
+            f"Users:\t{num_users}\n"
+            f"Basic Groups:\t{num_basic_groups}\n"
+            f"Super Groups:\t{num_super_groups}\n"
+            f"Channels:\t{num_channels}\n"
+            f"Bots:\t{num_bots}\n"
+            f"Blocked Chats:\t{num_blocked_chats}\n"
+            f"Obtained in {duration_in_seconds} seconds."
+        )
